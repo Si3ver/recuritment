6720b58cc9b2a69ac4b70ba21510eeed9c95c4a0
Si3ver <izhouwl@163.com>
2020-11-08 18:42:38 +0800

    fix typo: recuritment1 -> recuritment

--- recuritment/asgi.py
+++ recuritment/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for recuritment1 project.
+ASGI config for recuritment project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recuritment1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recuritment.settings')
 
 application = get_asgi_application()
